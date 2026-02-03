7b1773e41e98090d441534a35fe415de55c3cb33
CyberAxe3108 <125237722+CyberAxe3108@users.noreply.github.com>
2026-02-03 17:55:40 -0500

    Lab2: Using the random method for a guessing game

---
+++ Lab2.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter two numbers: ");
+String input1 = Console.ReadLine();
+String input2 = Console.ReadLine();
+Random rand = new Random();
+int num1 = rand.Next(int.Parse(input1), int.Parse(input2));
+Console.WriteLine("Guess the number between " + input1 + " and " + input2);
+int guess = int.Parse(Console.ReadLine());
+while (guess != num1)
+{
+    if (guess < num1)
+    {
+        Console.WriteLine("Too low! Try again.");
+    }
+    else
+    {
+        Console.WriteLine("Too high! Try again.");
+    }
+    guess = int.Parse(Console.ReadLine());
+}
